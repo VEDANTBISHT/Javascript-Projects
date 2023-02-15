80a157883627fa76d46e17b8a50410342083d43c
Vedant <vedantbisht111@gmail.com>
2023-02-15 23:37:08 +0530

    1st commit

---
+++ QuizApp/script.js
@@ -0,0 +1,45 @@
+const quizData =[
+    {
+        question: 'what is the capital of india',
+        a: 'Delhi',
+        b: 'lucknow',
+        c: 'chandigarh',
+        d: 'Mumbai',
+        correct: 'a'
+    },
+    {
+        question: 'the speed of lightis?',
+        a: '400000 km/sec',
+        b: '300000 km/sec',
+        c: '200000 km/sec',
+        d: '100000 km/sec',
+        correct: 'a'
+    },
+    {
+        question:'what is 2*2?',
+        a: '4',
+        b: '6',
+        c: '5',
+        d: '15',
+        correct: 'a'
+
+    },
+    {
+        question:'what is 4*2?',
+        a: '4',
+        b: '6',
+        c: '8',
+        d: '15',
+        correct: 'c'
+
+    },
+    {
+        question:'what is 8*2?',
+        a: '4',
+        b: '16',
+        c: '8',
+        d: '15',
+        correct: 'b'
+
+    },
+]
