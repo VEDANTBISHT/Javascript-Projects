d514d05325f56403b39f66c9631f9d5b3fba102a
Vedant <vedantbisht111@gmail.com>
2023-02-16 20:19:40 +0530

    js commit 1

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -42,4 +42,27 @@ const quizData =[
         correct: 'b'
 
     },
-]
+];
+
+
+const questionE1 =document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuiz =0;
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuiz = quizData[currentQuiz];    
+    questionE1.innerHTML = currentQuiz.
+    question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+    currentQuiz++;
+
+}
