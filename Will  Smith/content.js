ce78b51080a7332e0e2a526a1c0a342f54b14abe
VEDANTBISHT <vedantbisht111@gmail.com>
2023-03-31 19:13:24 +0530

    javascript added

---
+++ Will  Smith/content.js
@@ -0,0 +1,17 @@
+let Anya =[
+
+    "https://images4.alphacoders.com/118/thumbbig-1187146.webp",
+    "https://images5.alphacoders.com/124/thumbbig-1240551.webp",
+    "https://images3.alphacoders.com/119/thumbbig-1197606.webp",
+    "https://images4.alphacoders.com/119/thumbbig-1197605.webp",
+    "https://images8.alphacoders.com/124/thumbbig-1244496.webp",
+    "https://images2.alphacoders.com/124/thumbbig-1244257.webp",
+    "https://images5.alphacoders.com/129/thumbbig-1290746.webp",
+    "https://images6.alphacoders.com/126/thumbbig-1262201.webp",
+];
+
+const imgs =document.getElementsByTagName("img");
+for(let i=0; i<imgs.length; i++) {
+    const randomImg = Math.floor(Math.random() * Anya.length)
+    imgs[i].src =Anya[randomImgImg];
+}
