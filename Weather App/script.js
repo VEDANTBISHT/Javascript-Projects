7dbab7f9c917443062e76e8ce7fe0a8e744b6330
VEDANTBISHT <vedantbisht111@gmail.com>
2023-04-11 22:39:40 +0530

    .

--- Weather App/script.js
+++ Weather App/script.js
@@ -6,7 +6,6 @@ locationBtn = inputPart.querySelector("button"),
 weatherPart = wrapper.querySelector(".weather-part"),
 wIcon = weatherPart.querySelector("img"),
 arrowBack = wrapper.querySelector("header i");
-
 let api;
 
 inputField.addEventListener("keyup", e =>{
